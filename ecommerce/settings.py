0d19eef4e172d36596c1103dd8a3ed59c9e5c438
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-05-01 21:40:57 +0300

    add views not valadtion input and dd viewset in view and add urls in file urls and add restframwork in file setings as installed app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'products',
     'users',
+    'rest_framework',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from products.views  import CategoryViewSet, ProductViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+
 ]
