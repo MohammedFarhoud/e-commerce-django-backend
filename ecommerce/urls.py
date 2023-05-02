9edcb01e000ab236dce62909629c0f1b8707eaaa
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-05-02 19:06:34 +0300

    ProductListByCategory &&   productserach

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from products.views  import CategoryViewSet, ProductViewSet
+from products.views  import CategoryViewSet, ProductViewSet ,ProductListByCategory ,ProductList
 
 
 router = routers.DefaultRouter()
@@ -12,5 +12,9 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    
+    path('category/<int:category_id>/products', ProductListByCategory.as_view(), name='product_list_by_category'),
+    path('productserach/', ProductList.as_view(), name='product_list'),
+
 
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Category,Product
 from .serializers import CategorySerializer,ProductSerializer
+from rest_framework.generics import ListAPIView
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset=Category.objects.all()
@@ -10,4 +11,21 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
+
+
+class ProductListByCategory(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return Product.objects.filter(category__id=category_id)
     
+class ProductList(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        search_term = self.request.query_params.get('search', '')
+        if search_term:
+            return Product.objects.filter(name__icontains=search_term)
+        else:
+            return Product.objects.all()
