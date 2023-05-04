524236866324e18b41173cfeef0e86c0b41a3ca8
Mohammed Abdallah <mohammed.a.farhoud.com>
2023-05-04 17:17:59 +0300

    Adjust remove from cart route

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,5 @@ from cart.views import CartView
 
 urlpatterns = [
     path('', CartView.as_view(), name='cart'),
-    path('delete/<int:product>/', CartView.as_view(), name='remove_from_cart'),
+    path('<int:product>/', CartView.as_view(), name='remove_from_cart'),
 ]
