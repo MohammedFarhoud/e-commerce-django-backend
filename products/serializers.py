2bb0ae948a7df5135d50b16e1e3241cc52541072
Mohammed Abdallah <mohammed.a.farhoud.com>
2023-05-15 23:58:43 +0300

    Return whether product is in cart or whishlist or not with get request

--- products/serializers.py
+++ products/serializers.py
@@ -15,7 +15,17 @@ class ImageSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     images = ImageSerializer(many=True)
+    is_in_cart = serializers.SerializerMethodField()
+    is_in_wishlist = serializers.SerializerMethodField()
     
     class Meta:
         model=Product
-        fields = ['id', 'name', 'description', 'price', 'quantity', 'category', 'images']
+        fields = ['id', 'name', 'description', 'price', 'quantity', 'category', 'images', 'is_in_cart', 'is_in_wishlist']
+        
+    def get_is_in_cart(self, obj):
+        user = self.context['request'].user
+        return user.cart.products.filter(id=obj.id).exists()
+
+    def get_is_in_wishlist(self, obj):
+        user = self.context['request'].user
+        return user.wishlist.product.filter(id=obj.id).exists()
