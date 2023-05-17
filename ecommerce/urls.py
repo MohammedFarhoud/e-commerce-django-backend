90ab977a331658e2e8587b44e56b84f1aa78d19a
Mahmoudawd4 <mahmoudawd54@gmail.com>
2023-05-18 02:51:56 +0300

    edit payment

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from wishlist.views import WishlistList, WishlistDetail ,UserWishlistList ,WishlistItemDelete
 # from orders.views import PaymentView 
 # from orders.views import CreateCheckOutSession ,stripe_webhook_view
-from orders.views import PaymentView ,create_payment_intent ,process_payment ,my_webhook_view
+from payment.views import PaymentView ,my_webhook_view
 
 router = routers.DefaultRouter()
 router.register(r'category', CategoryViewSet)
@@ -28,17 +28,11 @@ urlpatterns = [
     path('user/wishlist', UserWishlistList.as_view(), name='user-wishlist-list'),
     path('wishlist/product/<int:id>/', WishlistItemDelete.as_view(), name='remove-from-wishlist'),
     
-    # path('api/payments/create-payment-intent/',create_payment_intent),
     path('payment/', PaymentView.as_view(), name='payment'),
     path('api/webhook', my_webhook_view, name='stripe_webhook'),
 
-    path('api/payments/create-payment-intent/', create_payment_intent, name='create_payment_intent'),
-    path('api/payments/', process_payment, name='process_payment'),
 
 
-    # path('create_checkout_session/', CreateCheckOutSession, name='create_checkout_session'),
-    # path('stripe_webhook/', stripe_webhook_view, name='stripe_webhook'),
-    # path('create-checkout-session/', views.CreateCheckOutSession, name='create_checkout_session'),
 
     
     path('orders/', include('orders.urls')),
